7a5674709a3256a91c16e8cb01d3505fd3a90afe
Taliba Sadiq <taliba.latif@gmail.com>
2024-05-23 22:56:01 -0400

    project 4 stage 3

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,24 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener;
+{
+  "clicks",
+    () => {
+      profileEditModal.classList.add("modal_opened");
+    };
+}
+
+const profileCloseButton = document.querySelector("#profile-close-button");
+
+profileEditButton.addEventListener;
+{
+  "clicks",
+    () => {
+      profileEditModal.classList.add("modal_close");
+    };
+}
